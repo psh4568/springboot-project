1f89994bc90fa64e31026619dd3f86c92d21ae14
Pooja Sharma <pooja199320@gmail.com>
2026-05-25 03:54:59 -0700

    Update ProductRequestDTO.java
    added open api annotations for better documentation / swagger ui

--- src/main/java/com/jwt/spring_security_jwtcasestudy/dto/ProductRequestDTO.java
+++ src/main/java/com/jwt/spring_security_jwtcasestudy/dto/ProductRequestDTO.java
@@ -1,20 +1,36 @@
 package com.jwt.spring_security_jwtcasestudy.dto;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
+// OpenAPI imports
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Request object for creating a product")
 public class ProductRequestDTO {
-/*
- * this class needed by Jackson for JSON conversion
- * Request DTO is used to accept client request data and incoming JSON into java objects.
- */
+
+    /*
+     * This class is used to receive product data from client (JSON → Java object)
+     */
+
+    @Schema(
+            description = "Name of the product",
+            example = "Laptop"
+    )
     @NotBlank
     private String name;
 
+    @Schema(
+            description = "Category of the product",
+            example = "Electronics"
+    )
     @NotBlank
     private String category;
 
+    @Schema(
+            description = "Price of the product (must be positive value)",
+            example = "50000"
+    )
     @Positive
     private Double price;
 
